4164ba247f237b33ef90c31e44ec7e006ea41027
Maithem <maithem.mun@gmail.com>
2017-06-16 20:48:24 -0700

    Hax

--- runtime/src/main/java/org/corfudb/runtime/recovery/ParallelReader.java
+++ runtime/src/main/java/org/corfudb/runtime/recovery/ParallelReader.java
@@ -19,19 +19,25 @@ import java.util.concurrent.ThreadFactory;
  * Created by maithem on 6/21/17.
  */
 public class ParallelReader {
-    private final CorfuRuntime runtime;
     private final BlockingQueue<Future<Segment>> queue;
-    private final int singleReadBatchSize = 50;
+    private final int singleReadBatchSize = 10;
     ThreadFactory producerTF = new ThreadFactoryBuilder()
             .setNameFormat("producer-%d").build();
     final ExecutorService producer = Executors.newSingleThreadExecutor(producerTF);
     final ExecutorService workers;
+    final CorfuRuntime[] runtimes;
+    final int numThreads;
 
 
-    public ParallelReader(int numThreads, CorfuRuntime runtime) {
-        this.runtime = runtime;
-        this.queue = new LinkedBlockingQueue(50);
+    public ParallelReader(int numThreads, String config) {
+        this.numThreads = numThreads;
+        this.queue = new LinkedBlockingQueue(100);
         this.workers = Executors.newWorkStealingPool(numThreads);
+        this.runtimes = new CorfuRuntime[numThreads];
+
+        for (int x = 0; x < numThreads; x++) {
+            runtimes[x] = new CorfuRuntime(config).connect();
+        }
     }
 
     public BlockingQueue<Future<Segment>> getQueue() {
@@ -41,12 +47,14 @@ public class ParallelReader {
     public Future read(long start, long end) {
         Runnable task = () -> {
             try {
+                int index = 0;
                 long a = start;
                 while (a <= end) {
                     long endPoint = Math.min(a + singleReadBatchSize, end);
                     SegmentRequest sr = new SegmentRequest(a, endPoint);
                     a = endPoint + 1;
-                    submitRead(sr);
+                    submitRead(sr, runtimes[index % numThreads]);
+                    index++;
                 }
 
                 Future<Segment> endSegment = CompletableFuture.completedFuture(Segment.END);
@@ -59,17 +67,17 @@ public class ParallelReader {
         return producer.submit(task);
     }
 
-    private void submitRead(SegmentRequest sr) throws InterruptedException {
+    private void submitRead(SegmentRequest sr, CorfuRuntime rt) throws InterruptedException {
         Callable<Segment> task = () -> {
             HashSet<Long> range = new HashSet();
             for (long x = sr.start; x <= sr.end; x++) {
                 range.add(x);
             }
-            Map<Long, ILogData> addresses = runtime.getAddressSpaceView().read(range);
+            Map<Long, ILogData> addresses = rt.getAddressSpaceView().cacheFetch(range);
             for (long a : addresses.keySet()) {
-                addresses.get(a).getPayload(runtime);
+                addresses.get(a).getPayload(rt);
             }
-            return new Segment(runtime.getAddressSpaceView().read(range));
+            return new Segment(addresses);
         };
 
         Future<Segment> ft = workers.submit(task);
--- test/src/test/java/org/corfudb/integration/StreamIT.java
+++ test/src/test/java/org/corfudb/integration/StreamIT.java
@@ -1,12 +1,20 @@
 package org.corfudb.integration;
 
+import com.google.gson.Gson;
 import org.corfudb.protocols.wireprotocol.ILogData;
 import org.corfudb.runtime.CorfuRuntime;
+import org.corfudb.runtime.recovery.ParallelReader;
+import org.corfudb.runtime.recovery.Segment;
 import org.corfudb.runtime.view.stream.IStreamView;
 import org.junit.Before;
+import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Set;
 import java.util.UUID;
+import java.util.concurrent.Future;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -14,55 +22,143 @@ import static org.assertj.core.api.Assertions.assertThat;
  * A set integration tests that exercise the stream API.
  */
 
-public class StreamIT extends AbstractIT {
-    static String corfuSingleNodeHost;
-    static int corfuSingleNodePort;
+public class StreamIT  {
 
-    @Before
-    public void loadProperties() throws Exception {
-        corfuSingleNodeHost = (String) PROPERTIES.get("corfuSingleNodeHost");
-        corfuSingleNodePort = Integer.parseInt((String) PROPERTIES.get("corfuSingleNodePort"));
+
+    @Test
+    public void testParallelRead() throws Exception {
+        ParallelReader parallelReader = new ParallelReader(4, "192.168.1.67:9000");
+
+        Future readerFt = parallelReader.read(0, 200000);
+
+        long addressesProcessed = 0;
+        long startTime = System.nanoTime();
+        while (true) {
+            Segment segment = parallelReader.getQueue().take().get();
+            if (segment == Segment.END) {
+                break;
+            }
+
+
+                //System.out.println("Processed address " + addressesProcessed);
+            Thread.sleep(50);
+            addressesProcessed += segment.addresses.size();
+        }
+        long stopTime = System.nanoTime();
+        System.out.println("Elapsed time: " + (stopTime - startTime));
+
+        System.out.println("Number of addresses processed: " + addressesProcessed);
     }
 
-//    @Test
+    @Test
     public void simpleStreamTest() throws Exception {
 
-        Process corfuServerProcess = new CorfuServerRunner()
-                .setHost(corfuSingleNodeHost)
-                .setPort(corfuSingleNodePort)
-                .runServer();
 
-        CorfuRuntime rt = createDefaultRuntime();
-        rt.setCacheDisabled(true);
+        Gson gson = new Gson();
+        String result = gson.toJson(new Payload());
+        byte[] dataJson = result.getBytes();
 
-        Random rand = new Random();
+        Payload rev = gson.fromJson(result, Payload.class);
 
-        UUID streamId = CorfuRuntime.getStreamID(Integer.toString(rand.nextInt()));
+        UUID streamId = CorfuRuntime.getStreamID("s1");
 
-        IStreamView s1 = rt.getStreamsView().get(streamId);
+        String configStr = "192.168.1.67:9000";
 
-        // Verify that the stream is empty
-        assertThat(s1.hasNext())
-                .isFalse();
 
-        // Generate and append random data
-        int entrySize = Integer.valueOf(PROPERTIES.getProperty("largeEntrySize"));
-        final int numEntries = 100;
-        byte[][] data = new byte[numEntries][entrySize];
+        int numThreads = 8 * 4;
+        int iterations = 2000000 / numThreads;
+        Thread[] threads = new Thread[numThreads];
 
-        for(int x = 0; x < numEntries; x++) {
-            rand.nextBytes(data[x]);
-            s1.append(data[x]);
+        long ss1 = System.currentTimeMillis();
+
+        for (int x = 0; x < numThreads; x++) {
+            CorfuRuntime rt = new CorfuRuntime(configStr).connect();
+            rt.setCacheDisabled(true);
+            IStreamView s1 = rt.getStreamsView().get(streamId);
+
+            threads[x] = new Thread(
+                    new Runnable() {
+                        public void run() {
+                            for (int x = 0; x < iterations; x++) {
+                                s1.append(dataJson);
+                            }
+                        }
+                    });
+
+            threads[x].start();
         }
+
+        for (int x = 0; x < numThreads; x++) {
+            threads[x].join();
+        }
+
+        long ss2 = System.currentTimeMillis();
+        System.out.print(ss2 - ss1);
 
         // Read back the data and verify it is correct
-        for(int x = 0; x < numEntries; x++) {
-            ILogData entry = s1.nextUpTo(x);
-            byte[] tmp = (byte[]) entry.getPayload(rt);
+        //for(int x = 0; x < numEntries; x++) {
+        // ILogData entry = s1.nextUpTo(x);
+        //  Payload tmp =  (Payload)entry.getPayload(rt);
 
-            assertThat(tmp).isEqualTo(data[x]);
+        // long ss3 = System.currentTimeMillis();
+
+
+        /*
+        CorfuRuntime rt = new CorfuRuntime(configStr).connect();
+        long s1 = System.currentTimeMillis();
+        long x = 0;
+        try {
+            for (x = 0; x < Long.MAX_VALUE; x++) {
+                ILogData data = rt.getAddressSpaceView().read(x);
+                byte[] buf = (byte[]) data.getPayload(rt);
+                String doc2 = new String(buf);
+                Payload obj = gson.fromJson(doc2, Payload.class);
+                int a = 0;
+                if(x % 10000 == 0) {
+                    System.out.println("reading at" + x);
+                }
+            }
+        } catch (RuntimeException e) {
+            System.out.println("adddress is " + x);
         }
 
-        assertThat(shutdownCorfuServer(corfuServerProcess)).isTrue();
+        long e1 = System.currentTimeMillis();
+        System.out.println(e1 - s1);
+        */
+    }
+
+
+    public class Payload {
+        private long f1 = 1;
+        private long f2 = 2;
+        private long f4 = 3;
+        private long f5 = 4;
+        private long f6 = 5;
+
+        private UUID f7 = new UUID(0, 0);
+        private UUID f8 = new UUID(0, 0);
+        private UUID f9 = new UUID(0, 0);
+        private UUID f10 = new UUID(0, 0);
+        private UUID f11 = new UUID(0, 0);
+
+        private String f12 = "This is a string, This is a string, This is a string, This is a string, This is a string";
+        private String f13 = "This is a string, This is a string, This is a string, This is a string, This is a string";
+        private String f14 = "This is a string, This is a string, This is a string, This is a string, This is a string";
+        private String f15 = "This is a string, This is a string, This is a string, This is a string, This is a string";
+        private String f16 = "This is a string, This is a string, This is a string, This is a string, This is a string";
+
+        private Set<String> set;
+        private HashMap<String, String> map;
+
+        public Payload() {
+            set = new HashSet();
+            map = new HashMap();
+
+            for(int x = 0; x < 100; x++) {
+                set.add("This is a string" + x);
+                map.put("key" + x, "string value");
+            }
+        }
+
     }
 }
